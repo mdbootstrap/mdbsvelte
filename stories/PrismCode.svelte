57ebc307e1cfbb9ed8c262ffdb250bea77b887bc
SauravKanchan <sauravnk30@gmail.com>
2020-05-08 15:48:29 +0530

    Add scroll in prismjs

--- stories/PrismCode.svelte
+++ stories/PrismCode.svelte
@@ -15,6 +15,14 @@
     Prism.highlightElement(_domNode, isAsync);
   });
 </script>
+<style>
+  pre {
+    overflow: auto;
+    word-wrap: normal;
+    white-space: pre;
+    max-height: 500px;
+  }
+</style>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.16.0/themes/prism-okaidia.min.css">
 
 <pre>
