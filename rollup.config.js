9aabe48c25ff197e51407239cd4662840af0a62e
SauravKanchan <sauravnk30@gmail.com>
2020-07-16 16:06:36 +0530

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
 import {terser} from 'rollup-plugin-terser';
+import analyze from 'rollup-plugin-analyzer'
+import autoPreprocess from 'svelte-preprocess'
 import pkg from './package.json';
+import bundleSize from 'rollup-plugin-bundle-size';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -28,54 +30,21 @@ export default {
     svelte({
       // enable run-time checks when not in production
       dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css: css => {
-        css.write('public/build/bundle.css');
-      }
+      // generate: production ? 'dom' : 'ssr',
+      hydratable: true,
+      preprocess: autoPreprocess({
+        postcss: {
+          plugins: [require('autoprefixer')()],
+        },
+      })
     }),
-
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration -
-    // consult the documentation for details:
-    // https://github.com/rollup/plugins/tree/master/packages/commonjs
-    resolve({
-      browser: true,
-      dedupe: ['svelte']
-    }),
+    resolve(),
     commonjs(),
-
-    // In dev mode, call `npm run start` once
-    // the bundle has been generated
-    !production && serve(),
-
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
-    !production && livereload('public'),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
-    production && terser()
+    production && terser(),
+    production && analyze(),
+    production && bundleSize(),
   ],
   watch: {
-    clearScreen: false
+    clearScreen: false,
   }
-};
-
-function serve() {
-  let started = false;
-
-  return {
-    writeBundle() {
-      if (!started) {
-        started = true;
-
-        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-          stdio: ['ignore', 'inherit', 'inherit'],
-          shell: true
-        });
-      }
-    }
-  };
 }
